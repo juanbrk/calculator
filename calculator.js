9ad31a15408fb5872bd78797415536ba7506da7e
JuanG <juangiacosabrk@gmail.com>
2020-11-05 02:30:24 -0300

    Solved sum:     - stores both operands and displays them accordingly     - when = is pressed, the result of sum is displayed and display is ready to receive new input

--- calculator.js
+++ calculator.js
@@ -2,9 +2,15 @@
 const display = document.querySelector('#display');
 const numbers = Array.from(document.getElementsByClassName('numeric'));
 const clearBtn = document.querySelector('#clear');
+const addBtn = document.querySelector('#add');
+const computeBtn = document.querySelector('#compute');
+
 
 //variables
 let zeroed = true; // when display awaits for input
+let operandA = null;
+let operandB = null;
+let isSum = false; // to refactor into an object of operations
 
 // consts
 
@@ -25,8 +31,40 @@ function addClickability(){
         updateDisplay(element.currentTarget.value);
      });
    }
-
+   
    clearBtn.addEventListener('click', ()  => clearDisplay());
+   addBtn.addEventListener('click', ()  => sum());
+   computeBtn.addEventListener('click', ()  => compute());
+
+}
+
+/**
+ * Make calculations
+ */
+function compute(){
+    let computedValue = null;
+    operandB = display.value;
+    if (isSum){
+        computedValue = Number(operandB) + Number(operandA);
+    }
+
+    zeroed = true;
+    updateDisplay(computedValue, true);
+    return computedValue;
+}
+
+
+function sum(){
+    if (!!operandA){
+        operandA += Number(display.value);
+        zeroed = true;
+        updateDisplay(operandA, true);
+    } else {
+        operandA = Number(display.value);
+    }
+
+    zeroed = true;
+    isSum = true;
 }
 
 
@@ -34,7 +72,7 @@ function addClickability(){
  * displays whatever was clicked
  * @param {string} value of the clicked button to be displayed
  */
-function updateDisplay(value){
+function updateDisplay(value, resetDisplay = false){
     if (zeroed){ // no input yet
         display.value = value;
         zeroed = false;
@@ -42,6 +80,8 @@ function updateDisplay(value){
         let displayValue = display.value;
         display.value = displayValue + value;
     }
+
+    zeroed = resetDisplay; 
 }
 // main
 clearDisplay();
